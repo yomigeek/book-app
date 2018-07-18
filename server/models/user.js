c57c5373d1d8ef592a1d166bdc8b745df1b654f7
Oyomi <seayomi@gmail.com>
2018-07-18 16:44:41 +0100

    ch(database-setup): postgresql and sequelize setup - setup sequelize postgresql orm - migrate sequelize tables to postgresql db

---
+++ server/models/user.js
@@ -0,0 +1,26 @@
+export default (sequelize, DataTypes) => {
+  const user = sequelize.define('user', {
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+
+  user.associate = (models) => {
+    user.hasMany(models.book, {
+      foreignKey: 'userId',
+      as: 'books',
+
+    });
+    // associations can be defined here
+  };
+  return user;
+};
