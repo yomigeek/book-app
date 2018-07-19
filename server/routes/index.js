a09eb0f0a7bebb8a3a02826dae7176bbbbcb4afc
Sinmiloluwa Oloyede <sinmiloluwaoloyede@Sinmiloluwas-MacBook-Pro.local>
2018-07-19 23:00:27 +0100

    added update

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import UserController from '../controller/UserController';
 import userSignUpValidate from '../validations/UserAccount';
+import bookController from '../controller/bookController';
 
 const routes = express();
 
 // Sign user up
 routes.post('/signup', userSignUpValidate, UserController.userSignUp);
 routes.post('/login', UserController.userLogin);
+routes.put('/users/books/:bookId', bookController.createBook);
 
 export default routes;
