c8fba8b7dea09aca9af520425542696c67f151ed
Kalu Nwanna <nwannauk@gmail.com>
2018-07-20 11:13:29 +0100

    add login in route

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,11 +5,7 @@ import userSignUpValidate from '../validations/UserAccount';
 const routes = express();
 
 // Sign user up
-<<<<<<< HEAD
 routes.post('/signup', userSignUpValidate, UserController.userSignUp);
-=======
-routes.post('/signup', UserController.userSignUp);
 routes.post('/login', UserController.userLogin);
->>>>>>> ft(user-login): implement login
 
 export default routes;
